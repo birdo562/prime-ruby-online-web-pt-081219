55c2afa6a95e2cf172a633affc5ac8a5eb7affa8
Roberto Medina <robby.medina562@gmail.com>
2019-08-24 07:10:09 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(number)
   first=2
   if number > 1
     range = (first..number-1).to_a
-    range.none? do |test_n| 
+    range.none? do |test_number| 
       number % test_n ==0
     end
   else
